7e00389ace5a726bf7b92d4a7efe9c98ef60ed1f
vin <alvintasuncion22@gmail.com>
2026-09-09 22:28:35 +0800

    new comiitmet

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#This is me.
+#Python Tools ver 1.0
 
-print("Hey what's your name?")
+def main_loop():
+    print("Welcome to Python Tools ver 1.0")
+    choice = int(input("Please select an option:\n1. Image viewer\n2. Unit converter\n3. Exit\n4."))
 
-name. = input("Enter your name:")
 
-print(f"Hey wassup {name}!")
+
+
+main_loop()
