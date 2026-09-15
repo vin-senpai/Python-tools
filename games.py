f486b89ef9341ddc9460878abf1d1eabcb35a4f3
vin <alvintasuncion22@gmail.com>
2026-09-15 23:46:16 +0800

    Added some final polishing touch and added ascii design.

--- games.py
+++ games.py
@@ -3,12 +3,28 @@ import os
 
 
 def main():
-    games = ["1.Snake","2.TIC TAC TOE","3.Tetris","4.Exit"]
+    
+    games = ["1.TIC TAC TOE"]
+    print(r""" 
+    __________________|      |____________________________________________
+     ,--.    ,--.          ,--.   ,--.
+    |oo  | _  \  `.       | oo | |  oo|
+o  o|~~  |(_) /   ;       | ~~ | |  ~~|o  o  o  o  o  o  o  o  o  o  o
+    |/\/\|   '._,'        |/\/\| |/\/\|
+__________________        ____________________________________________
+                  |      |
+_______  _______  __   __  _______  _______ 
+|       ||   _   ||  |_|  ||       ||       |
+|    ___||  |_|  ||       ||    ___||  _____|
+|   | __ |       ||       ||   |___ | |_____ 
+|   ||  ||       ||       ||    ___||_____  |
+|   |_| ||   _   || ||_|| ||   |___  _____| |
+|_______||__| |__||_|   |_||_______||_______|""")
     print("Choose a Game: ")
     for i in games:
         print(i)
     choice = int(input(""))
-    if(choice == 2):
+    if(choice == 1):
         print("Welcome to tic tac toes!")
         tictac()
     else:
@@ -111,8 +127,7 @@ class tictac:
     def mainloop(self):
         while not self.gameover == True:
             #Get the user input.
-
-            
+            self.draw_grid()
             self.check_grid()
             self.p1x = int(input("Enter Player 1 x position(1-3): "))
             self.p1y = int(input("Enter Player 1 y position(1-3): "))   
@@ -127,7 +142,7 @@ class tictac:
           
             self.grid[self.p2y-1][self.p2x-1] = 2
 
-            self.draw_grid()
+           
 
 
 
--- main.py
+++ main.py
@@ -4,13 +4,40 @@ from games import main
 
 
 def main_loop():
-    print("Welcome to Python Tools ver 1.0")
-    choice = int(input("Please select an option:\n1. Games\n2. Unit converter\n3. Exit\n4."))
+    #Raw string
+    design = r"""",--------------------------===---.
+| ACT-1B                          |
+| ,----------------------------.  |         
+| |  Welcome to python toolbox  | |
+| |                             | |
+| |                             | |         
+| |  _.~"(_.~"(_.~"(_.~"(_.~"(  | |             
+| |                             | |                ____        __  __                   __              __    
+| |                             | |               / __ \__  __/ /_/ /_  ____  ____     / /_____  ____  / /____
+| |                             | |              / /_/ / / / / __/ __ \/ __ \/ __ \   / __/ __ \/ __ \/ / ___/
+| |                             | |             / ____/ /_/ / /_/ / / / /_/ / / / /  / /_/ /_/ / /_/ / (__  ) 
+| |                             | |            /_/    \__, /\__/_/ /_/\____/_/ /_/   \__/\____/\____/_/____/  
+| |                             | |                 /____/                                                     """
 
+    
+    
+    #print("Welcome to Python Tools ver 1.0")
+    print(design)
+    print("| | Please select an option:    | |\n| | 1. Games\t\t\t| |\n| | 2. Unit converter\t\t| |\n| | 3. Exit\t\t\t| |\t")
+    
+    choice = int(input(r"""| |.............................| |
+| | |_| :.)        .    (.: |_| | |
+| '-----....._________.....-----' |
+|     _    _     O     _    _     |
+|    |_|  |_|    |    |_|  |_|    |
+"------......____O____......------ ===>""" ))
+   
+    
     if choice == 1:
         print("\n" * 200)
         main()
     elif choice == 2:
+        print("\n" * 200)
         unit_converter()
     elif choice == 3:
         os._exit(0)
@@ -21,7 +48,7 @@ def main_loop():
 
 
 def unit_converter():
-    conversionlist = ["1. Length", "2. Weight", "3. Temperature"]
+    conversionlist = ["1. Length", "2. Weight"]
     lengthunitlist = ["1. Millimeter", "2. Centimeter", "3. Meter", "4. Kilometer", "5. Micrometer", "6. Nanometer", "7. Inches", "8. Foot", "9. Yards", "10. Miles"]
     weightunitlist = ["1. Microgram","2. Milligram","3. Gram","4. Kilogram","5. MetricTon"]
     funit = 0
@@ -64,9 +91,23 @@ def unit_converter():
 
     }
 
+    
+    print(r""" _____________________
+|  _________________  |
+| |              67.| |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|""")
     print("WELCOME TO UNIT CONVERTER")
     print("Please select the type of conversion: ")
-    
     for i in conversionlist: 
         print(i)
 
@@ -78,51 +119,56 @@ def unit_converter():
 
         for i in lengthunitlist:
             print(i)
+
         funit = int(input(""))
-        print(lengthunitlist[funit-1])
         print("Enter unit to convert to: ")
 
         for i in lengthunitlist:
             print(i)
 
         sunit = int(input(""))
+        if(funit >= 11 or sunit >= 11):
+            print("ERROR Wrong Value!")
+            os._exit(0)
         value = float(input("Enter value to convert: "))
 
         #Convert the value into the smallest unit first(Base unit) and then convert it into desired unit.
         if(funit <= 6 and sunit <= 6): #Standard Metric system.
             value_in_mm = value * standard_metric_unit[lengthunitlist[funit-1].strip('1234567890.').lower().strip()]
             final_value = value_in_mm / standard_metric_unit[lengthunitlist[sunit-1].strip('1234567890.').lower().strip()]
-            print("i used metric")
+            #print("i used metric")
         elif(funit >= 7 and sunit >= 7): #Imperial metric system.
             value_in_inches = value * imperial_metric_unit[lengthunitlist[funit-1].strip('123456789.').lower().strip()]
             final_value = value_in_inches / imperial_metric_unit[lengthunitlist[sunit-1].strip('1234567890.').lower().strip()]
             print(f"{lengthunitlist[funit-1].strip('1234567890.').lower().strip()} to inches is: {final_value}")
-            print("i used imperial")
+            #print("i used imperial")
         #Inorder to convert from imperial to metric(or vice versa),we must first con    vert them into their lowest value and convert both lowest value to desired unit.
         elif(funit <= 6 and sunit >= 7):
             value_in_mm = value * standard_metric_unit[lengthunitlist[funit-1].strip('1234567890.').lower().strip()]
             value_in_inches = value_in_mm * standard_metric_unit[lengthunitlist[sunit-1].strip('1234567890.').lower().strip()]
             final_value = value_in_inches   
-            print("std to imp")
+            #print("std to imp")
         elif(funit >= 7 and sunit <= 6):
             value_in_inches = value * imperial_metric_unit[lengthunitlist[funit-1].strip('123456789.').lower().strip()]
             value_in_mm = value_in_inches *imperial_metric_unit[lengthunitlist[sunit-1].strip('123456789.').lower().strip()]
             final_value = value_in_mm
-            print("imp to std")
-        else:
-            final_value = value * standard_metric_unit[lengthunitlist[sunit-1].strip('1234567890.').lower().strip()]
-            print("ERROR wrong")
-            print("i used thiss")
-        
-        if(funit == 1 or sunit == 1):
-            formatted_c_value =  f"{final_value:,.10f}".rstrip("0")
-            print("i formatted it")
-        else:
-            formatted_c_value =  f"{final_value:,.10f}".rstrip("0")
-            print("i didn NOT formatted it")
-           
+            #print("imp to std")
+        else: 
+            #final_value = value * standard_metric_unit[lengthunitlist[sunit-1].strip('1234567890.').lower().strip()]
+            print("ERROR Invalid output!")
+            #print("i used thiss")
         #make a function so that it will only use scientific notation once the value_in_mm varible reaches upto 8 digits/or when converting 0.9cm to meters.
+        formatted_c_value =  f"{final_value:,.10f}".rstrip("0")
 
+        # if(funit == 1 or sunit == 1):
+        #     formatted_c_value =  f"{final_value:,.10f}".rstrip("0")
+        #     print("i formatted it")
+        # else:
+        #     formatted_c_value =  f"{final_value:,.10f}".rstrip("0")
+        #     print("i didn NOT formatted it")
+           
+      
+        #Print final value.
         print(f"value from {lengthunitlist[funit-1].strip('1234567890.').lower().strip()}({value}) to {lengthunitlist[sunit-1].strip("01234567890.").lower().strip()}: {formatted_c_value}" )
         input("Press a Key to exit")
         print("\n" * 150)
@@ -130,6 +176,7 @@ def unit_converter():
         #print(f"Converting from {lengthunitlist[funit-1].strip('12345678910.')} to {lengthunitlist[sunit-1].strip('12345678910.')}") 
 
         #print(f"{lengthunitlist[funit-1].strip('12345678910.')} = {kilometer} km")
+
     #Weigth convertre
     elif choice == 2:
         #Ask user for inputs
@@ -142,7 +189,9 @@ def unit_converter():
         for i in weightunitlist:
             print(i)
         sunit = int(input(''))
-
+        if(sunit >= 6 or funit >= 6):
+            print("Error invalid option!")
+            os._exit(0)
         w_value = float(input("Enter value to convert: "))
 
         #Just like from the length measurement, we convert the units to their lowest value
@@ -153,7 +202,7 @@ def unit_converter():
         #Format its value so it wont use scientific notation.(Just keep it from now even it doest have a purpose yet.)
         formatted_w_value = weighted_converted
        
-        print(f"value from{weightunitlist[funit-1].strip('1234567890.')} to {weightunitlist[sunit-1].strip('1234567890.')}: {round(formatted_w_value):,}")
+        print(f"value from{weightunitlist[funit-1].strip('1234567890.')}({w_value}) to {weightunitlist[sunit-1].strip('1234567890.')}: {round(formatted_w_value,9):,}")
 
     
         
